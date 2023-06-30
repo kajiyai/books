ca2850b342aec20753c10d2b451bb7cfd75993fa
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-07-01 08:46:37 +0900

    singletonパターンの例インスタンス化xxxxp8

---
+++ SoftwareMistakesandTradeoffs/Singleton.java
@@ -0,0 +1,16 @@
+// 注釈がついてました.....
+// 訳注 https://refactoring.guru/ja/design-patterns/singletonただしGoF本の著者陣は、このパターンを同書の改訂
+// 時に最初に削除すべきものとして挙げていました。テストがしにくくなるなどの弊害も大きく、よく推奨しないとも
+// 言われますが、本章ではAPIと実装の設計の良し悪しを見るための事例として割り切ってお読みください。
+
+public class Singleton {
+  private static Singleton instance;
+  private Singleton() {}
+
+  public static Singleton getInstance() {
+    if (instance == null) {
+      instance = new Singleton();
+    }
+    return instance;
+  }
+}
